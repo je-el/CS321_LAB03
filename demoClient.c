2feaba8022f388d5055118ed4b887dfc1d3df87f
LisaJacklin <lmjacklin@alaska.edu>
2023-03-15 11:50:30 -0800

    more notes
    https://gist.github.com/Abhey/47e09377a527acfc2480dbc5515df872
    
    this link has one way we could possibly go about our server/client connection through a single function/main within the program rather than two functions for each client. This would also allow more than two clients if we wanted by the looks of it.

--- demoClient.c
+++ demoClient.c
@@ -2,6 +2,17 @@
 //Group: Lisa Jacklin, Jewel Maldonado
 //client file
 
+/*
+Objective: 
+1. create two different clients
+2. read messages from the user input rather than them being hardcoded
+3. the server should forward the accepted message from one client to the next
+4.should only work properly when two clients are up
+5.server should end when one client sends BYE.
+6. cannot be aborted by hitting control-c
+7.assume both client and server are one the same machine
+8. send client pids to sever to establish connections
+*/
 
 // Client side C/C++ program to demonstrate Socket programming
 #include <stdio.h>
@@ -9,18 +20,16 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <string.h>
-#define PORT 8080					//sets port number
+#define PORT 8080                                
    
 int main(int argc, char const *argv[])
 {
-    int sock = 0, valread;				//variables required;
-    
-    struct sockaddr_in serv_addr;		//socket addressing 
-    //sending first msg with a buffer for spacing
+    int sock = 0, valread;                      //starting at socket zero
+    struct sockaddr_in serv_addr;               //creating a structure for socket addresses in server address        
+
     char *hello = "Hello from client";
     char buffer[1024] = {0};
-
-    //checks to see if the socket was created with the file descriptor (socket())
+    //if the socket does not actually get created (thus equal 0 or less)
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
         printf("\n Socket creation error \n");
@@ -30,34 +39,51 @@ int main(int argc, char const *argv[])
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
        
-    //another set of fail safes, first for an incorrect address, next for issues with connection
     // Convert IPv4 and IPv6 addresses from text to binary form
     if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) 
     {
         printf("\nInvalid address/ Address not supported \n");
         return -1;
     }
-   
+    //checks the connection between the socket and the server through server address.
     if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
     {
         printf("\nConnection Failed \n");
         return -1;
     }
+    else {
+        //within here, data can be sent back and forth from client/server
+        printf("\n Connection Established.\n");
+    }
 
-    //when all failsafes are passed, a message between client and server can be sent.
-    //sends the message to the server
-    send(sock , hello , strlen(hello) , 0 );
+    send(sock , hello , strlen(hello) , 0 );      //think like signal sent, signal here being hello from the socket to the server
+
     printf("Hello message sent\n");
-	
-    //reads the response from the socket or client
-    valread = read( sock , buffer, 1024);
+    valread = read( sock , buffer, 1024);         //reads the socket and space/buffer before the carriage return
     printf("%s\n",buffer );
     return 0;
 }
 
-//outside of main, two functions, one for each client
+//Client 1
 void ClientOne() {
+
+    /* one idea to communicate through messages from user
+    * char data[1024];
+		int read = recv(clientSocket,data,1024,0);
+		data[read] = '\0';
+		printf("%s\n",data);
+
+    * char input[1024];
+    * scanf("%s", input);
+    * send (clientSocket, input, 1024, 0);
+    * 
+    * read = recv(clientSocket,data,1024, 0);
+    * printf("%s\n", data);
+     
+    */
 }
 
+//Client 2
 void ClientTwo() {
+
 }
