9ca3fab03fe6187a137f4755ca367ce06866e3eb
je-el <maldojeweln@gmail.com>
2023-03-16 22:34:41 -0800

    Had to change out all use cases of vector because the program refused to compile in C. When changing to C++ the code threw many errors

--- Server.c
+++ Server.c
@@ -1,10 +1,10 @@
 //CS321_Lab03
 //Group: Lisa Jacklin, Jewel Maldonado
 //Server file
-//language: C++
+//language: C
 
 
-// Server side C/C++ program to demonstrate Socket programming
+// Server side C program to demonstrate Socket programming
 // including necessary header files for socket programming
 #include <unistd.h>
 #include <stdio.h>
@@ -12,8 +12,7 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
-#include <vector>
-#include <algorithm>
+#include <stdbool.h>
 
 //define the port number as PORT 8080
 #define PORT 8080
@@ -21,112 +20,124 @@
 //main :)
 int main(int argc, char const *argv[])
 {
-    //variables needed
-    int server_fd, new_socket, valread;
-    struct sockaddr_in address; // specifies a socket address 
-    				// 
-    int opt = 1;
-    int addrlen = sizeof(address);
-    char buffer[1024] = {0};
-    char *hello = "Hello from server";
-    std::vector<int> clients;
-    std::vector<std::string> client_names;
+        //variables needed
+        int server_fd, new_socket, valread;
+        struct sockaddr_in address; // specifies a socket address
+                        //
+        int opt = 1;
+        int addrlen = sizeof(address);
+        char buffer[1024] = {0};
+        char *hello = "Hello from server";
+        int max_clients = 30;
+        int clients[max_clients];
+        char client_names[max_clients][100];
+        bool client_connected[max_clients];
 
-    // Creating socket file descriptor using the socket() function
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-    {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
-    }
-       
-    // Forcefully attaching socket to the port 8080
-    // set the socket options using setsocketopt() function to allow 
-    // resue of the port number
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
-                                                  &opt, sizeof(opt)))
-    {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
-    //set up the server address structure by specifying
-    //the address family
-    address.sin_family = AF_INET;
-    //IP Address
-    address.sin_addr.s_addr = INADDR_ANY;
-    //and the port number
-    address.sin_port = htons( PORT );
-       
-    // Forcefully attaching socket to the port 8080
-    //binding the socket to the address using bind()
-    if (bind(server_fd, (struct sockaddr *)&address, 
-                                 sizeof(address))<0)
-    {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
-    }
-    //listen for incoming connections using listen()
-    if (listen(server_fd, 3) < 0)
-    {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
-    //accept incoming connection using accept()
-    while(1)
-    { //while loop added
-    	if ((new_socket = accept(server_fd, (struct sockaddr *)&address,(socklen_t*)&addrlen))<0)
-    	{
-    	    perror("accept");
-    	    exit(EXIT_FAILURE);
-    	}
-    	//connecting clients
-    	clients.push_back(new_socket);
-    	printf("New client connected. Total clients: %d\n, clients.size());
-    	char* welcome = "Hello and Welcome to the Chat Server!";
-	send(new_socket, welcome, strlen(welcome), 0);
-    	while(1){
-		memset(buffer, 0, sizeof(buffer));
-		valread = read(new_socket, buffer, 1024);
-		if(valread ==0){
-			//client has disconnected
-			printf("Client %s disconnected. Total Clients: %d\n",client_names[new_socket], clients.size()-1);
-			clients.erase(std::remove(clients.begin(), clients.end(), new_socket), clients.end());
-			client_names.erase(client_names.begin() + (clients.size()));
-			break;
-		}
-		// checking for command that changes client name
-		if (buffer[0] == '/'){
-			char* new_name = strtok(buffer + 1, " \n");
-			if (new_name != NULL) {
-				//extracting the name provided from user
-				int index = std::find(clients.begin(), clients.end(), new_socket) - clients.begin();
-				client_names[index] = new_name;
-				printf("Client %d renamed to %s\n", index, new_name);
-			}
-		}else {
+        //initialize clients and client_connected arrays
+        for (int i = 0; i < max_clients; i++) {
+            clients[i] = 0;
+            client_connected[i] = false;
+        }
 
-			//send message to all clients
-			std::string message = client_names[std::find(clients.begin(), clients.end(), new_socket) - clients.begin()] + ": " + buffer;
-			for (int i = 0; i < clients.size(); i++){
-				//if (clients[i], buffer, strlen(buffer), 0);
-				if (clients[i] != new_socket){
-				       send(clients[i],	message.c_str(), message.length(), 0);
-			//there was a reason why I used the c_str() function but I forgot why exactly I needed it compared to just reading the string ....
-				}
+        // Creating socket file descriptor using the socket() function
+        if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+        {
+            perror("socket failed");
+            exit(EXIT_FAILURE);
+        }
 
-			}
-		}
-		
-	}
-}
-    
+        // Forcefully attaching socket to the port 8080
+        // set the socket options using setsocketopt() function to allow
+        // resue of the port number
+        if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+                                                      &opt, sizeof(opt)))
+        {
+            perror("setsockopt");
+            exit(EXIT_FAILURE);
+        }
+        //set up the server address structure by specifying
+        //the address family
+        address.sin_family = AF_INET;
+        //IP Address
+        address.sin_addr.s_addr = INADDR_ANY;
+        //and the port number
+        address.sin_port = htons( PORT );
 
-    //read data from the socket using read() function and 
-    //print it to the console
-    //valread = read( new_socket , buffer, 1024);
-    //printf("%s\n",buffer );
-    // send a response to the client using send()
-    //print message indication th emessage was sent
-    //send(new_socket , hello , strlen(hello) , 0 );
-    //printf("Hello message sent\n");
+        // Forcefully attaching socket to the port 8080
+        //binding the socket to the address using bind()
+        if (bind(server_fd, (struct sockaddr *)&address,
+                                     sizeof(address))<0)
+        {
+            perror("bind failed");
+            exit(EXIT_FAILURE);
+        }
+        //listen for incoming connections using listen()
+        if (listen(server_fd, 3) < 0)
+        {
+            perror("listen");
+            exit(EXIT_FAILURE);
+        }
+        //accept incoming connection using accept()
+        while(1)
+        { //while loop added
+            if ((new_socket = accept(server_fd, (struct sockaddr *)&address,(socklen_t*)&addrlen))<0)
+            {
+                perror("accept");
+                exit(EXIT_FAILURE);
+            }
+            //connecting clients
+            for (int i = 0; i < max_clients; i++) {
+                if (clients[i] == 0) {
+                    clients[i] = new_socket;
+                    printf("New client connected. Total clients: %d\\n", i+1);
+                    char* welcome = "Hello and Welcome to the Chat Server!";
+                    send(new_socket, welcome, strlen(welcome), 0);
+                    client_connected[i] = true;
+                    break;
+                }
+            }
+            while(1){
+            memset(buffer, 0, sizeof(buffer));
+            valread = read(new_socket, buffer, 1024);
+            if(valread ==0){
+                //client has disconnected
+                for (int i = 0; i < max_clients; i++) {
+                    if (clients[i] == new_socket) {
+                        printf("Client %s disconnected. Total Clients: %d\\n", client_names[i], i);
+                        client_connected[i] = false;
+                        clients[i] = 0;
+                        break;
+                    }
+                }
+                break;
+            }
+            // checking for command that changes client name
+            if (buffer[0] == '/'){
+                char* new_name = strtok(buffer + 1, " \\n");
+                if (new_name != NULL) {
+                    //extracting the name provided from user
+                    for (int i = 0; i < max_clients; i++) {
+                        if (clients[i] == new_socket) {
+                            strncpy(client_names[i], new_name, 100);
+                            printf("Client %d renamed to %s\\n", i, new_name);
+                            break;
+                        }
+                    }
+                }
+            }else {
+
+                //send message to all clients
+                for (int i = 0; i < max_clients; i++) {
+                    if (clients[i] != 0 && clients[i] != new_socket && client_connected[i]) {
+                        char message[1124];
+                        snprintf(message, 1124, "%s: %s", client_names[new_socket-4], buffer);
+                        send(clients[i],    message, strlen(message), 0);
+                    }
+                }
+            }
+
+        }
+    }
     return 0;
 }
+
