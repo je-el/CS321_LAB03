f289b5004419095a933ba8f1875ed8d23f4d26a7
je-el <maldojeweln@gmail.com>
2023-03-20 13:29:17 -0800

    added suggestions in comments

--- NewClient.c
+++ NewClient.c
@@ -29,7 +29,7 @@ void client(char *name, int client_num)
 {
     int sock = 0, valread;
     struct sockaddr_in serv_addr;              
-
+    pid_t pid = getpid(); // getting the process ID of the client
     char buffer[1024] = {0};
 
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
@@ -51,37 +51,79 @@ void client(char *name, int client_num)
     {
         printf("\nConnection Failed \n");
         return;
-    }
-    else {
+    } else {
         printf("\n Connection Established...\n");
    
-    //within here, since connection has been established, a 
-    //code block can be executed to print data.
+    	//within here, since connection has been established, a 
+    	//code block can be executed to print data.
+	
+	//send the PID to the server // adding this in so that we can send and recieve the PID to the server
+	//one way to do this is:
+	/*//this will also establish chatting between the clients too!
+	char pid_str[1024];
+        sprintf(pid_str, "%d", pid); // convert pid to string
 
+        // send pid to server
+        send(sock, pid_str, strlen(pid_str), 0);
+   
+        // receive welcome message from server
+        char welcome_msg[1024];
+        valread = recv(sock, welcome_msg, 1024, 0);
+        welcome_msg[valread] = '\0';
+        printf("%s\n", welcome_msg);
+   
+        // chat loop
+        char msg[1024];
+        while(1) {
+            printf("Client %d Chatting: ", pid);
+            fgets(msg, 1024, stdin);
+            send(sock , msg , strlen(msg) , 0 );
+            if(strncmp(msg, "BYE", 3) == 0) {
+                printf("Closing connection...\n");
+                break;
+            }
+            valread = recv(sock, buffer, 1024, 0);
+            buffer[valread] = '\0';
+            printf("Client %d received: %s\n", pid, buffer);
+        }
+	//also see in main how the clients will show the different pid's of each client
+	*/
+	
+	//this is another way to do it
+	/*
+	char pid_msg[1024];
+	snprintf(pid_msg, sizeof(pid_msg), "PID:%d", pid);
+	send(sock, pid_msg, strlen(pid_msg), 0);
+	//recieve a message from the server
+	int reading = recv(sock, buffer, 1024, 0);
+        buffer[read] = '\0';
+        printf("%s\n", buffer);
+	//then you start chatting with the while loop
+	*/
 
-     //one idea to communicate through messages from user
-     char data[1024];
-		int read = recv(sock,data,1024,0);
-		data[read] = '\0';
+     	//one idea to communicate through messages from user
+     	char data[1024];
+		int byte_read = recv(sock,data,1024,0);
+		data[byte_read] = '\0';
 		printf("Client: %s\n",data);
 
-     char input[1024];
-     scanf("%s", input);
-     send (sock, input, 1024, 0);
+     	char input[1024];
+     		scanf("%s", input);
+     		send (sock, input, 1024, 0);
      
-     int byte_read = recv(sock,data,1024, 0);
-     printf("Client:%s\n", data);
+     		byte_read = recv(sock,data,1024, 0);
+     		printf("Client:%s\n", data);
      
 
-    char msg[1024];
-    while(1) {
-        printf("Client %d: %s",  client_num,name);
-        fgets(msg, 1024, stdin);
-        send(sock , msg , strlen(msg) , 0 );
-        if(strncmp(msg, "BYE", 3) == 0) {
-            printf("Closing connection...\n");
-            break;
-        }
+    	char msg[1024];
+    	while(1) {
+        	printf("Client %d: %s",  client_num,name);
+        	fgets(msg, 1024, stdin);
+        	send(sock , msg , strlen(msg) , 0 );
+        	if(strncmp(msg, "BYE", 3) == 0) {
+            		printf("Closing connection...\n");
+            		break;
+        	}
         valread = byte_read;
         printf("%s\n",buffer );
     }
@@ -109,7 +151,13 @@ int main(int argc, char const *argv[])
     char name2[1024];
     int cli1 = 1, cli2 = 2;
     //get_client_name(name, 1024);
-    
+    //lets use the pid's to show the names of each client rather than using client "1" client "2" idea.
+    /*
+    pid_t pid1 = getpid();
+    client(pid1);
+    pid_t pid2 = getpid();
+    client(pid2);
+    */
     client(name,cli1);
     client(name2,cli2);
 
